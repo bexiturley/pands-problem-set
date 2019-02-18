d20466451104e63d690487e4d612779c558bdd08
Rebecca Turley <bexiturley@gmail.com>
2019-02-18 14:57:36 +0000

    Created Solution 3

---
+++ pythondivisors.py
@@ -0,0 +1,14 @@
+# Solution to problem 3
+# Rebecca Turley, 2019-02-13
+# python divisors.py
+
+nl = []
+for x in range (1000, 10000) :
+
+    if (x % 6 == 0)  and (x % 12 != 0) :
+
+       print(x) 
+       
+#nl.append (str (x) )
+
+#print (','.join (nl) )
