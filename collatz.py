69d16d96895ec0b60d5fe1ae12e8526f5ebab21b
Rebecca Turley <bexiturley@gmail.com>
2019-03-27 17:09:35 +0000

    Added another print condition to problem 4-collatz so the last figure it prints out is 1

--- collatz.py
+++ collatz.py
@@ -6,7 +6,9 @@
 
 # The programme invites the user to enter a number.
 n = float (input ("Please enter a number "))
-  
+
+
+
 while n != 1 :
 # The condition states that while n (the number entered in the previous step by the user) is not equal to 1 so the steps will be run until 1 is reached.
 
@@ -17,7 +19,7 @@ while n != 1 :
     else:                 # n is not divisible by 2 so is odd, if this is the case multiply by 3 and add 1.
       n = ((n * 3) + 1)
 
-
+print (1.0)
 # I cant get it to print 1 at the end of the answer as I have set the condition to end at 1.  I can not get it to work otherwise. I have tried changing it to less than 1, 0.5. Setting the input as integer rather than a float but to no avail.
 # Adapted from code https://stackoverflow.com/questions/41299440/python-how-to-break-while-loop-after-empty-value-in-a-int-turning-input 
 
